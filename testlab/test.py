fcac74c8e519f2cee7403c55e2b971bb9b282b08
Valentin Heinrich <v.heinrich99@gmail.com>
2023-01-08 13:42:48 +0100

    Added IPv6 support for test environment
    Tests now run inside a virtual machine, which is created and configured
    by vagrant. The testscript now relies on ipmininet, which provides
    automatic routing and builtin IPv6 support.

--- testlab/test.py
+++ testlab/test.py
@@ -15,247 +15,71 @@ You should have received a copy of the GNU General Public License along with Aug
 If not, see <https://www.gnu.org/licenses/>.
 """
 
-import itertools
-import functools
-from collections.abc import Iterable
-from ipaddress import IPv4Network
-from subprocess import TimeoutExpired
-
-from mininet.topo import Topo
-from mininet.net import Mininet
-from mininet.node import Node, Controller
-from mininet.log import setLogLevel
-from mininet.link import Intf
+import time
+import sys
+from signal import SIGINT
+from configparser import ConfigParser
+from ipmininet.iptopo import IPTopo
+from ipmininet.ipnet import IPNet
+from ipmininet.cli import IPCLI
 
 
-# Code from: https://docs.python.org/3.9/library/itertools.html
-def pairwise(iterable: Iterable) -> zip:
-    a, b = itertools.tee(iterable)
-    next(b, None)
-    return zip(a, b)
+class DiamondTopo(IPTopo):
+
+    def build(self, *args, **kwargs):
+        x1, x2, u1, u2, m1, m2, l1, l2 = self.addRouters("x1", "x2", "u1", "u2", "m1", "m2", "l1", "l2")
+        client = self.addHost("client")
+        server = self.addHost("server")
+
+        self.addLinks((x1,u1),(x1,m1),(x1,l1))
+        self.addLinks((u1,u2),(m1,m2),(l1,l2))
+        self.addLinks((u2,x2),(m2,x2),(l2,x2))
+        self.addLinks((client,x1),(x2,server))
+
+        super().build(*args, **kwargs)
 
 
-class TestNode(Node):
-    """A mininet node without ICMP rate limiting."""
+    def post_build(self, net):
+        for r in self.routers():
+            net[r].cmd("sysctl net.ipv4.fib_multipath_hash_policy=1")
+            net[r].cmd("sysctl net.ipv6.fib_multipath_hash_policy=1")
 
-    def config(self, **params):
-        super().config(**params)
-        self.cmd("sysctl net.ipv4.icmp_ratelimit=0")
+        super().post_build(net)
 
-    def terminate(self):
-        self.cmd("sysctl net.ipv4.icmp_ratelimit=1000")
-        super().terminate()
-
-
-# Code from: https://github.com/mininet/mininet/blob/master/examples/linuxrouter.py
-class Router(TestNode):
-    """A mininet node with per-flow load balancing and forwarding."""
-
-    def config(self, **params):
-        super().config(**params)
-        self.cmd("sysctl net.ipv4.ip_forward=1")
-        self.cmd("sysctl net.ipv4.fib_multipath_hash_policy=1")
-
-    def terminate(self):
-        self.cmd("sysctl net.ipv4.ip_forward=0")
-        self.cmd("sysctl net.ipv4.fib_multipath_hash_policy=0")
-        super().terminate()
-
-
-class DiamondTopo(Topo):
-    """A simple diamond topology with 2 equal-length branches in the middle."""
-
-    def build(self, nrouters, **kwargs):
-        assert nrouters > 0
-
-        addRouter = functools.partial(self.addNode, cls=Router, **kwargs)
-        addHost = functools.partial(self.addHost, cls=TestNode, **kwargs)
-
-        start_router = addRouter("start", ip="10.0.1.1/24")
-        end_router = addRouter("end", ip="11.0.1.1/24")
-        upper_routers = [
-            addRouter(f"upper{i}", ip=f"12.0.{i+1}.1/24") for i in range(nrouters)
-        ]
-        lower_routers = [
-            addRouter(f"lower{i}", ip=f"13.0.{i+1}.1/24") for i in range(nrouters)
-        ]
-
-        client = addHost("client", ip="10.0.1.100/24", **kwargs)
-        server = addHost("server", ip="11.0.1.100/24", **kwargs)
-
-        # Connect client to start.
-        self.addLink(client, start_router)
-        self.addLink(server, end_router)
-
-        self.addLink(start_router, upper_routers[0], params1={"ip": "12.0.1.2/24"})
-        self.addLink(start_router, lower_routers[0], params1={"ip": "13.0.1.2/24"})
-
-        # Upper section.
-        for i, (u1, u2) in enumerate(itertools.pairwise(upper_routers)):
-            self.addLink(u1, u2, params1={"ip": f"12.0.{i+2}.2/24"})
-
-        # Lower section.
-        for i, (l1, l2) in enumerate(pairwise(lower_routers)):
-            self.addLink(l1, l2, params1={"ip": f"13.0.{i+2}.2/24"})
-
-        self.addLink(
-            end_router,
-            upper_routers[-1],
-            params1={"ip": f"12.0.{len(upper_routers)+1}.1/24"},
-            params2={"ip": f"12.0.{len(upper_routers)+1}.2/24"},
-        )
-        self.addLink(
-            end_router,
-            lower_routers[-1],
-            params1={"ip": f"13.0.{len(lower_routers)+1}.1/24"},
-            params2={"ip": f"13.0.{len(lower_routers)+1}.2/24"},
-        )
-
-
-def connectedTo(intf: Intf) -> Intf:
-    """Returns the peer interface."""
-    link = intf.link
-    i1, i2 = link.intf1, link.intf2
-    return i1 if intf == i2 else i2
-
-
-def intf_network(intf: Intf) -> IPv4Network:
-    """Returns the network associated with a given interface."""
-    return IPv4Network(f"{intf.IP()}/{intf.prefixLen}", strict=False)
-
-
-def node_networks(node: Node) -> list[IPv4Network]:
-    """Returns all networks associated with the nodes interfaces."""
-    return [intf_network(i) for i in node.intfList()]
-
-
-def intf_routes(
-    intf: Intf, excludes: set[IPv4Network] = None, current_weight: int = 1
-) -> dict[IPv4Network, int]:
-    """Returns the networks reachable from interface with a given weight.
-    The shortest weight is chosen if multiple weights are possible for a given network.
-    """
-    if excludes is None:
-        excludes = set()
-    else:
-        excludes = set(excludes)
-    excludes.add(intf)
-
-    networks = {net: current_weight for net in node_networks(intf.node)}
-
-    next_node = connectedTo(intf).node
-
-    for next_intf in set(next_node.intfList()) - excludes:
-        next_routes = intf_routes(next_intf, excludes, current_weight + 1)
-        for net, weight in next_routes.items():
-            # If a network entry already exists we choose the one with the least weight.
-            if net in networks:
-                weight = min(networks[net], weight)
-            networks[net] = weight
-
-    return networks
-
-
-def node_routes(node: Node) -> dict[IPv4Network, list[Intf]]:
-    """Aggregates the routes for each interface of a node.
-    If a network is reachable over multiple interfaces with the same weight,
-    a list of those interfaces is returned.
-    """
-    net_from_intf = {}
-    net_to_weight = {}
-
-    local_nets = {intf_network(i) for i in node.intfList()}
-
-    for intf in node.intfList():
-        for net, weight in intf_routes(intf, excludes=local_nets).items():
-            if net in net_from_intf:
-                if weight > net_to_weight[net]:
-                    continue
-                elif weight == net_to_weight[net]:
-                    net_from_intf[net].append(intf)
-                    continue
-
-            net_from_intf[net] = [intf]
-            net_to_weight[net] = weight
-
-    return net_from_intf
-
-
-def configure_routes(node: Node, routes: dict[IPv4Network, list[Intf]]):
-    """Configures the routing table of a node based on the routes.
-    The routes are determined by a call to node_routes.
-    """
-    node.cmd("ip route flush all")
-
-    for intf in node.intfList():
-        node.cmd(f"ip route add {connectedTo(intf).IP()} dev {intf}")
-
-    for net, interfaces in routes.items():
-        node.cmd(
-            f"ip route add {net} "
-            + " ".join(f"nexthop via {connectedTo(intf).IP()}" for intf in interfaces)
-        )
-
-
-def run():
-    topo = DiamondTopo(3)
-    net = Mininet(
-        topo=topo,
-        waitConnected=True,
-    )
-    net.start()
-
-    for node in net.values():
-        # We skip the controller as we don't use it.
-        if isinstance(node, Controller):
-            continue
-        routes = node_routes(node)
-        configure_routes(node, routes)
-
-    open_processes = []
-    create_process = lambda node, cmd: open_processes.append(node.popen(cmd))
-
-    try:
-        net["lower0"].cmdPrint("iptables -A OUTPUT -p icmp -j DROP")
-        net["upper1"].cmdPrint("iptables -A OUTPUT -p icmp -j DROP")
-        create_process(net["end"], "tcpdump -i any -w end.pcap")
-
-        client, server = net["client"], net["server"]
-        create_process(
-            server, "./server/augsburg-traceroute-server -n 50000 -t 1000000000 server-eth0"
-        )
-
-        client.cmdPrint(
-            f"augsburg-traceroute -s -l debug -o 'tcp' --inter 0 --timeout 1 --abort 3 two-way tcp multipath --retry 2 {server.IP()}"
-        )
-        client.cmdPrint(
-            f"augsburg-traceroute -s -l debug -o 'udp' --inter 0 --timeout 1 --abort 3 two-way udp multipath --retry 2 {server.IP()}"
-        )
-        client.cmdPrint(
-            f"augsburg-traceroute -s -l debug -o 'icmp' --inter 0 --timeout 1 --abort 3 two-way icmp multipath --retry 2 {server.IP()}"
-        )
-
-        client.cmdPrint(
-            f"augsburg-traceroute -s -l debug -o 'tcp_single' --inter 0 --timeout 1 --abort 3 two-way tcp singlepath --flow 80 {server.IP()}"
-        )
-        client.cmdPrint(
-            f"augsburg-traceroute -s -l debug -o 'udp_single' --inter 0 --timeout 1 --abort 3 two-way udp singlepath --flow 53 {server.IP()}"
-        )
-        client.cmdPrint(
-            f"augsburg-traceroute -s -l debug -o 'icmp_single' --inter 0 --timeout 1 --abort 3 two-way icmp singlepath --flow 1919 {server.IP()}"
-        )
-
-    finally:
-        for p in open_processes:
-            try:
-                p.terminate()
-                p.wait(timeout=1)
-            except TimeoutExpired:
-                p.kill()
-
-    net.stop()
 
 
 if __name__ == "__main__":
-    setLogLevel("info")
-    run()
+    assert len(sys.argv) == 2
+    cfg_path = sys.argv[1]
+
+    cfg_parser = ConfigParser()
+    cfg_parser.read(cfg_path)
+
+    cfg = cfg_parser["TESTLAB"]
+
+    client = cfg["client"]
+    server_v4 = cfg["server_v4"]
+    server_v6 = cfg["server_v6"]
+
+    net = IPNet(topo=DiamondTopo())
+    try:
+        net.start()
+
+        popen_v4 = net["server"].popen(server_v4, "server-eth0")
+        popen_v6 = net["server"].popen(server_v6, "server-eth0")
+
+        print("Waiting 30 seconds for routes to converge..")
+        time.sleep(30)
+
+        for af in ["4","6"]:
+            for proto in ["udp", "tcp", "icmp"]:
+                print(f"Running test with IPv{af} and protocol {proto}")
+                popen_pcap = net["server"].popen("tcpdump", "-i", "server-eth0", "-w", f"{proto}_{af}.pcap")
+                net["client"].cmd(f"{client} -o {proto}_{af} -{af} reverse {proto} multipath server")
+                popen_pcap.send_signal(SIGINT)
+                time.sleep(0.5)
+
+        popen_v4.send_signal(SIGINT)
+        popen_v6.send_signal(SIGINT)
+    finally:
+        net.stop()
