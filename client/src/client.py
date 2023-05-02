b9515cb80a99e2b437f8a3814fdb21b74bc3a86e
Teun Vink <teun@teun.tv>
2023-05-03 00:27:24 +0200

    silly eol spacing

--- client/src/client.py
+++ client/src/client.py
@@ -9,10 +9,10 @@ either version 3 of the License, or (at your option) any later version.
 
 Augsburg-Traceroute is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
 without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-See the GNU General Public License for more details. 
+See the GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License along with Augsburg-Traceroute.
-If not, see <https://www.gnu.org/licenses/>.
+If not, see <https://www.gnu.org/licenses/>. 
 """
 
 import logging
